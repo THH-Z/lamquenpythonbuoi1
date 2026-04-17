deca5bcec2dc84469de38ded6a7754fd06ac4659
THH-Z <truonghoanghuy27032006@gmail.com>
2026-04-17 10:12:06 +0700

    Create thuchanhpython1.py

---
+++ thuchanhpython1.py
@@ -0,0 +1,15 @@
+print ("Nhap chieu dai cua hinh hop chu nhat: ");
+n = float(input('nhap gia tri chieu dai: '));
+print ("Nhap chieu rong cua hinh hop chu nhat: ");
+m = float(input('nhap gia tri chieu rong: '));
+print ("Nhap chieu cao cua hinh hop chu nhat: ");
+h = float(input('nhap gia tri chieu cao: '));
+
+
+sole = input('so le cua hinh hop chu nhat: ');
+dinhdang = '{:.'+sole+'f}';  #hien thi so le phia sau dua tren so len mìnhh nhap vao
+
+
+
+print ("dien tich day hinh chu nhat =",float(dinhdang.format(n * m)),"cm\u00b2");
+print ("dien tich hinh khoi =",float(dinhdang.format(n * m * h)),"cm\u00b2");
