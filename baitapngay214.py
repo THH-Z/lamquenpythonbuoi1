85e87987bd7330de0dff6ec7ac26443145c8a5bf
THH-Z <truonghoanghuy27032006@gmail.com>
2026-04-21 09:26:01 +0700

    dem tu ai

---
+++ baitapngay214.py
@@ -0,0 +1,11 @@
+s = '''hai Chiều chiều trước bến Văn Lâu 
+       Ai ngồi, ai câu, ai sầu, ai thảm 
+       Ai thương, ai cảm, ai nhớ, ai trông 
+       Thuyền ai thấp thoáng ven sông 
+       Đưa câu mái vẩy chạnh lòng nước non '''
+       
+#print("Nhap chuoi: ");
+#chuoi = input();
+print("Chuoi vua nhap la: " + s);
+print("Do dai chuoi la: " + str(len(s)));
+print(s.count(" ai"));
