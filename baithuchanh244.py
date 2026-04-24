ee64949d315cf66cd0ba9a582c3aa771e04b9818
THH-Z <truonghoanghuy27032006@gmail.com>
2026-04-24 09:53:37 +0700

    n+15

--- baithuchanh244.py
+++ baithuchanh244.py
@@ -4,3 +4,7 @@ n = int(input());
 giatrituyetdoi = lambda n: abs(n);
 
 print("gia tri tuyet doi cua", n, "la:", giatrituyetdoi(n));
+
+socong = lambda n: n + 15;
+print("so cong them 15 cua", n, "la:", socong(n));
+
