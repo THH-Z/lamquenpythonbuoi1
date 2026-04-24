d58e492823d6e1451320208c4a073dee19bdc75c
THH-Z <truonghoanghuy27032006@gmail.com>
2026-04-24 09:57:43 +0700

    boi so cua 13 và 19

--- baithuchanh244.py
+++ baithuchanh244.py
@@ -1,10 +1,23 @@
 print("nhap so nguyen: ");
 n = int(input());
 
+#a)
 giatrituyetdoi = lambda n: abs(n);
-
 print("gia tri tuyet doi cua", n, "la:", giatrituyetdoi(n));
 
+#b)
 socong = lambda n: n + 15;
 print("so cong them 15 cua", n, "la:", socong(n));
 
+#c)
+print("nhap so nguyen x: ");
+x = int(input());
+print("nhap so nguyen y: ");
+y = int(input());
+sotich = lambda x, y: x * y;
+print("so tich cua", x, "va", y, "la:", sotich(x, y));
+
+#d)
+boiso = lambda n: n % 13 != 0 and n % 19 != 0;
+if boiso(n):
+    print(n, "khong chia het cho 13 va 19");
